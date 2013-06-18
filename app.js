868eef20ab49af801768fbda0674ef60a8e18e29
Jon Packer <contact@jonpacker.com>
2013-06-18 15:16:19 +0200

    Don't cache

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get(/^\/(\d+)[^\d](\d+)/, function(req, res) {
   getimg(req.params[0], req.params[1], function(err, img) {
     if (err) return res.send(500, err.message);
     res.set('Content-Type', 'image/jpeg');
+    res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
     img.pipe(res);
   });
 });
